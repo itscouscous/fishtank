5e6e3cbf1efa2c9f4fd050b5c16e06003dee7f0a
YanfuOu <122131796+YanfuOu@users.noreply.github.com>
2025-04-15 03:20:34 -0400

    Update 4.2receiver.ino

--- 4.2receiver.ino
+++ 4.2receiver.ino
@@ -1,11 +1,7 @@
 /*
-Demo of slider for MEAM5100 2025
-Jessica Weakly
-(C)2024 all rights reserved
-*/
-
-/////////// UDP init ///////////
-
+ * sends text udp packet to TargetIP
+ *  using station mode (needs router) and static IP
+ */
 #include <WiFi.h>
 #include <WiFiUdp.h>
 // GM
@@ -15,31 +11,13 @@ const char* password = "12488674";
 // const char* ssid     = "esesyno";
 // const char* password = "eniacpenn";
 
-WiFiUDP UDPServer;
-//Use same code for both ESP32s, just swap the IP addresses
-IPAddress myIPaddress(192, 168, 1, 109); // change to your IP
-IPAddress target(192, 168, 1, 109); // change to IP you are sending to
-
-char udpBuffer[100]; //buffer can hold up to 100 characters
-
-/////////// VAR & PINS init ///////////
-
-// #define WHEEL1_PWM 1
-// #define WHEEL1_DIR1 10
-// #define WHEEL1_DIR2 8
-
-// #define WHEEL2_PWM 0
-// #define WHEEL2_DIR1 7
-// #define WHEEL2_DIR2 6
-
-//x
-#define WHEEL1_PWM 1
-#define WHEEL1_DIR1 10
-#define WHEEL1_DIR2 8
+#define WHEEL1_PWM 0
+#define WHEEL1_DIR1 7
+#define WHEEL1_DIR2 6
 //y
-#define WHEEL2_PWM 0
-#define WHEEL2_DIR1 7
-#define WHEEL2_DIR2 6
+#define WHEEL2_PWM 1
+#define WHEEL2_DIR1 10
+#define WHEEL2_DIR2 8
 
 // variables for PWM init
 const int freq = 30;      // Frequency in Hz
@@ -61,40 +39,40 @@ const int range_y = 100;
 const int dzy_max = deadzone_y + range_y;
 const int dzy_min = deadzone_y - range_y;
 
-/////////// FUNCTIONS ///////////
+WiFiUDP UDPServer;
+
+IPAddress target(192, 168, 1, 129); 
+IPAddress myIP(192, 168, 1, 124);
+
+uint8_t udpBuffer[100];
+
+volatile uint16_t x = 0; 
+volatile uint16_t y = 0; 
+
+volatile int duty_y;
+volatile int duty_x;
 
 void handleUDPServer() {
-  uint8_t packetBuffer[100]; //send int up to 100 characters long
-  int cb = UDPServer.parsePacket();
-  if (cb) { //if there is anything in the packet run this code
-
-    // int len = UDPServer.read(packetBuffer, 100);
-    // packetBuffer[len]=0;
-    // Serial.printf ("%s\n",packetBuffer);
-    // int please = atoi((char*)packetBuffer); //convert contents of packet buffer into an int
-    // if(please < 25){
-    //   ledcWrite(outputguy, 0);
-    // }else{ //use Ledc to modulate LED brightness based on potentiometer value
-    //   ledcWrite(outputguy, please);
-    // }
-
-    int len = UDPServer.read(udpBuffer, sizeof(udpBuffer) - 1);
-    if (len > 0) {
-      udpBuffer[len] = '\0';  // Null-terminate the received string
+  uint8_t packetBuffer[100];
+  int i, cb = UDPServer.parsePacket();
+  if (cb) {
+    UDPServer.read(packetBuffer, 100);
+    i = packetBuffer[0] + (packetBuffer[1] << 8); // puts 2 bytes into int
+    uint32_t Duty;
+    if(i >= 32767) {
+      y = i - 32767;
     }
-
-    Serial.printf("Received UDP Packet: %s\n", udpBuffer);
-
-    // Parse the received "x,y" data
-    int x, y;
-    if (sscanf(udpBuffer, "%d,%d", &x, &y) == 2) {
-
-      Serial.printf("Parsed X: %d, Y: %d\n", x, y);
-      makeCarGo(x, y);
-
-    } else {
-      Serial.println("Failed to parse UDP data!");
+    else {
+      x = i; 
     }
+    Serial.print("x: ");
+    Serial.print(x);
+
+    Serial.print(" y: "); // prints the number (note no need to convert to asii)
+    Serial.println(y);
+
+    makeCarGo(x, y); 
+
   }
 }
 
@@ -104,128 +82,67 @@ void makeCarGo(int x, int y){
     //if x is between dzx_max~4095, dutycycle map 0:4095 and dzx_max:4095 and DIRECTION 1
     //if x is between 0~dzx_max, dutycycle map 0:4095 and dzx_max~0 (MAKE SURE ITS INVERSED) and DIRECTION 2
 
+
+
   if(y <= dzy_max && y >= dzy_min){
-    ledcWrite(WHEEL2_PWM, 0);
-    Serial.print("\n WHEEL2 duty: 0");
+    duty_y = 0; 
+    Serial.print("\n in duty y deadzone");
   }else if(y > dzy_max){
-    int duty_y = map(y, dzy_max, 4095, 0, 4095);
-    ledcWrite(WHEEL2_PWM, duty_y);
-    
+    duty_y = map(y, dzy_max, 4095, 0, 4095);    
     digitalWrite(WHEEL2_DIR1, LOW);
     digitalWrite(WHEEL2_DIR2, HIGH);
-    
-    Serial.print("\n WHEEL2 dir1, duty_y:");
-    Serial.println(duty_y);
-
   }else if(y < dzy_min){
-    int duty_y = map(y, dzy_min, 0, 0, 4095);
-    ledcWrite(WHEEL2_PWM, duty_y);
-    
+    duty_y = map(y, dzy_min, 0, 0, 4095);    
     digitalWrite(WHEEL2_DIR2, LOW);
     digitalWrite(WHEEL2_DIR1, HIGH);
-
-    Serial.print("\n WHEEL2 dir2, duty_y:");
-    Serial.println(duty_y);
     
   }
 
 
   if(x <= dzx_max && x >= dzx_min){
-    ledcWrite(WHEEL1_PWM, 0);
-    Serial.print("\n WHEEL1 duty: 0");
+    duty_x = 0; 
+    Serial.print("\n in duty x deadzone");
   }else if(x > dzx_max){
-    int duty_x = map(x, dzx_max, 4095, 0, 4095);
-    ledcWrite(WHEEL1_PWM, duty_x);
-    
+    duty_x = map(x, dzx_max, 4095, 0, 4095);  
     digitalWrite(WHEEL1_DIR1, LOW);
     digitalWrite(WHEEL1_DIR2, HIGH);
-    
-    Serial.print("\n WHEEL1 dir1, duty_x:");
-    Serial.println(duty_x);
-
   }else if(x < dzx_min){
-    int duty_x = map(x, dzx_min, 0, 0, 4095);
-    ledcWrite(WHEEL1_PWM, duty_x);
-    
+    duty_x = map(x, dzx_min, 0, 0, 4095);
     digitalWrite(WHEEL1_DIR2, LOW);
     digitalWrite(WHEEL1_DIR1, HIGH);
-
-    Serial.print("\n WHEEL1 dir2, duty_x:");
-    Serial.println(duty_x);
-    
   }
-  
-  // if(y <= dzy_max && y >= dzy_min){
-  //   ledcWrite(WHEEL2_PWM, 0);
-  //   Serial.print("\n WHEEL2 duty: 0");
-  // }else if(y > dzy_max){
-  //   int duty_y = map(y, dzy_max, 4095, 0, 4095);
-  //   ledcWrite(WHEEL2_PWM, duty_y);
-    
-  //   digitalWrite(WHEEL2_DIR1, LOW);
-  //   digitalWrite(WHEEL2_DIR2, HIGH);
-    
-  //   Serial.print("\n WHEEL2 dir1, duty_y:");
-  //   Serial.println(duty_y);
 
-  // }else if(y < dzy_min){
-  //   int duty_y = map(y, dzy_min, 0, 0, 4095);
-  //   ledcWrite(WHEEL2_PWM, duty_y);
-    
-  //   digitalWrite(WHEEL2_DIR2, LOW);
-  //   digitalWrite(WHEEL2_DIR1, HIGH);
+  Serial.print("\n duty_x:");
+  Serial.println(duty_x);
+  Serial.print("\n duty_y:");
+  Serial.println(duty_y);
 
-  //   Serial.print("\n WHEEL2 dir2, duty_y:");
-  //   Serial.println(duty_y);
-    
-  // }
-  
+
+  ledcWrite(WHEEL1_PWM, duty_x);
+  ledcWrite(WHEEL2_PWM, duty_y);
+ 
 }
 
+
+
+
 void setup() {
-
   Serial.begin(115200);
-  analogReadResolution(12);
 
-  //WIFI Setup
+
   WiFi.begin(ssid, password);
 
-  WiFi.config( myIPaddress,        // Device IP address
-      IPAddress(192, 168, 1, 1),   // gateway (not important for 5100)
-      IPAddress(255, 255, 255, 0)); // net mask 
+  WiFi.config( myIP,    // device IP address
+      IPAddress(192, 168, 1, 1), // gateway (not used)
+      IPAddress(255, 255, 255, 0)); // netmask
   
-  UDPServer.begin(2808);  // 2808 arbitrary UDP port# need to use same one   
+  UDPServer.begin(2808);  // 2808 arbitrary UDP port#    
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);    Serial.print(".");
   }
-  Serial.println("WiFi connected");
-  Serial.print("Using static IP "); 
-  Serial.println(myIPaddress);
-  Serial.printf("WiFi connected to %s", ssid);
-
-  Serial.print(" to "); Serial.println(target);  
-
-  // WHEEL 1 init
-  ledcAttach(WHEEL1_PWM, freq, resolution);
-  ledcWrite(WHEEL1_PWM, 4095);
-  pinMode(WHEEL1_DIR1,OUTPUT);
-  pinMode(WHEEL1_DIR2,OUTPUT);
-
-  // WHEEL 2 init
-  ledcAttach(WHEEL2_PWM, freq, resolution);
-  ledcWrite(WHEEL2_PWM, 4095);
-  pinMode(WHEEL2_DIR1,OUTPUT);
-  pinMode(WHEEL2_DIR2,OUTPUT);
-
 }
 
 void loop() {
-//   Serial.println("loop1");
-  // h.serve();
-  // delay(10);
-
-  handleUDPServer(); //handle incoming packets
+  handleUDPServer();
   delay(10);
-
 }
-
